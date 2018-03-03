9529841a4064754df462a6a92f97492aa45fed0a
paurban <purban@teksystems.com>
2018-03-02 22:11:41 -0500

    Gulp Deployment

---
+++ src/Feature/xConnectRules/code/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace Sitecore.Feature.xConnectRules
+{
+    using Sitecore.Data;
+
+    public struct Constants
+    {
+
+  
+        public struct UserProfile
+        {
+            public struct Fields
+            {
+               
+            }
+        }
+    }
+}
